c0a4fcee1bc9bb901dd519424acc8ce8e7781b4c
YVic95 <vika.yevlentieva@gmail.com>
2024-01-29 17:59:13 +0200

    Basics of using routes in laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get(uri: '/welcome-page', action: function () {
+    return response(content:'<h2> Welcome on our awesome page! </h2>', status: 200)
+        ->header(key: 'Content-Type', values: 'text/plain')
+        ->header(key: 'foo', values: 'custom-value');
+});
+
+Route::get(uri: '/posts/{id}', action: function ($id) {
+    /**
+     * dd() - is a helper method for debugging
+     */
+    //dd($id);
+    return response(content: 'This is ' . $id . 'th post');
+})->where(name:'id', expression: '[0-9]+');
+
+Route::get(uri: '/search', action: function (Request $request) {
+    //dd(vars: $request);
+    return 'Welcome, ' . $request->user . ' from ' . $request->city . '!';
 });

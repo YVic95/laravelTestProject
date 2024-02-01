4cbc769ef08cebbc9469ede99c574e09f24c251b
YVic95 <vika.yevlentieva@gmail.com>
2024-02-01 16:04:11 +0200

    Add route a for whole list of listin and a rout fe for displaying single listing. All parameters moved to Listing model and fetches from the model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,24 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('listings', [
         'heading' => 'These are the latest listings!',
-        'listings' => [
-            [
-                'id' => '1',
-                'title' => 'Listing One',
-                'description' => 'Spanish (español) or Castilian (castellano) 
-                    is a Romance language of the Indo-European language family 
-                    that evolved from the Vulgar Latin spoken on the Iberian 
-                    Peninsula of Europe'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Listing Two',
-                'description' => 'Romanian is a Romance language, belonging to 
-                    the Italic branch of the Indo-European language family, 
-                    having much in common with languages such as Italian, 
-                    Spanish, French and Portuguese. Compared with the other Romance 
-                    languages, the closest relative of Romanian is Italian.'
-            ]
-        ]
+        //fetching parameters from the Eloquent Model
+        'listings' => Listing::getAll()
+    ]);
+});
+
+Route::get('/listing/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::getSingle($id)
     ]);
 });

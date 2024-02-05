7717a3a301b9b024014276364fd4ed443f0ff4dd
YVic95 <vika.yevlentieva@gmail.com>
2024-02-05 17:09:01 +0200

    Add main layout with site header and content container + Extend layout and add content section to listings view

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LaraProject</title>
+</head>
+<body>
+    <h1>Header of the site will be here!</h1>
+    {{--VIEW OUTPUT--}}
+    <div class="container">
+        @yield('listing-content')
+    </div>
+</body>
+</html>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,7 +1,10 @@
-<h1>{{$heading}}</h1>
-@forelse ($listings as $listing)
-    <h2><a href="/listing/{{$listing['id']}}">{{$listing['title']}}</a></h2>
-    <p>{{$listing['description']}}</p>
-@empty
-    <h2>No listings found!</h2>
-@endforelse
+@extends('layout')
+@section('listing-content')
+    <h1>{{$heading}}</h1>
+    @forelse ($listings as $listing)
+        <h2><a href="/listing/{{$listing['id']}}">{{$listing['title']}}</a></h2>
+        <p>{{$listing['description']}}</p>
+    @empty
+        <h2>No listings found!</h2>
+    @endforelse
+@endsection

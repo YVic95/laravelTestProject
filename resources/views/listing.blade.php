26d31b5bac000152e11a5abcdd661fd10f3e1001
YVic95 <vika.yevlentieva@gmail.com>
2024-02-16 10:12:15 +0200

    Markup for  and styles ingle listing page

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,6 +1,47 @@
 @extends('layout')
 @section('listing-content')
     @include('partials._search')
-    <h1 class="text-3xl font-bold underline">{{$listing['title']}}</h1>
-    <p>{{$listing['description']}}</p>
+    <a href="/" class="text-lg font-bold hover:underline ml-4 mb-4">
+        <i class="fa-solid fa-circle-arrow-left mr-2"></i>
+        Back
+    </a>
+    <div class="mx-4">
+        <div class="bg-gray-50 border border-gray-400 rounded p-10 mb-32">
+            <div class="flex flex-col align-center justify-center text-center items-center">
+                <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image.png')}}" alt="">
+                <h3 class="text-2xl mb-2">{{$listing->title}}</h1>
+                <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                <ul class="flex align-center justify-center">
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">Laravel</a>
+                    </li>
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">Backend</a>
+                    </li>
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">API</a>
+                    </li>
+                    <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                        <a href="#">Vue</a>
+                    </li>
+                </ul>
+                <div class="text-xl mt-4 mb-4"><i class="fa-solid fa-location-dot mr-1"></i>{{$listing->location}}</div>
+                <div class="border border-gray-200 w-full mb-6"></div>
+                <div>
+                    <h4 class="text-2xl font-bold mb-4">Job Description</h4>
+                    <div class="text-lg space-y-6 flex flex-col items-center">
+                        <p>{{$listing->description}}</p>
+                        <a class="block w-full sm:w-48 bg-red-600 hover:bg-black text-white rounded py-2 px-3 mr-1 text-base" href="mailto:{{$listing->email}}">
+                            <i class="fa-regular fa-envelope mr-2"></i>
+                            Contact employer
+                        </a>
+                        <a class="block w-full sm:w-48 bg-red-600 hover:bg-black text-white rounded py-2 px-3 mr-1 text-base" href="{{$listing->website}}" target="_blank">
+                            <i class="fa-solid fa-globe mr-2"></i>
+                            Visit Website
+                        </a>
+                    </div> 
+                </div>      
+            </div>
+        </div>
+    </div> 
 @endsection

6fcbe7b772022b3514a60ebb3876d171054de034
YVic95 <vika.yevlentieva@gmail.com>
2024-03-01 18:02:47 +0200

    Add mass assignment rule for specific table columns

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Listing extends Model
 {
     use HasFactory;
+    /**
+     * The attributes (table columns) that are mass assignable.
+     */
+    protected $fillable = ['company', 'title', 'location', 'website', 'email', 'tags', 'description'];
 
     /**
      * Scope a query to only include listings of a given type (by tag).
